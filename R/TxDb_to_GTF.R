c30f10ee4edbb0b601def03b39993a2ef07b9e34
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-02-03 00:02:46 -0500

    Update TxDb_to_GTF.R

--- R/TxDb_to_GTF.R
+++ R/TxDb_to_GTF.R
@@ -13,14 +13,15 @@ TxDb_to_GTF <- function(GRL,txdbTables) {
   initTab <- merge(txdbTables$transcripts,txdbTables$genes,by="tx_id",all=TRUE)
   initTab <- initTab[!duplicated(initTab$tx_name),]
   Tab <- merge(initTab,exonsdf,by.x="tx_name",by.y="group_name",all=TRUE)
-  Tab$gene_id <- paste("gene_id",Tab$gene_id,sep= " ")
-  Tab$tx_name <- paste("transcript_id",Tab$tx_name,sep= " ")
-  Tab$exon_rank <- paste("exon_number",Tab$exon_rank,sep= " ")
+  Tab$gene_id <- paste('gene_id "',Tab$gene_id,'";',sep= '')
+  Tab$tx_name <- paste('transcript_id "',Tab$tx_name,'";',sep= '')
+  Tab$exon_rank <- paste('exon_number "',Tab$exon_rank,'";',sep= '')
+  Tab$exon_id <- paste('exon_id "',Tab$exon_id,'";',sep= '')
   #Tab$tx_chrom <- gsub("\\chr","",Tab$tx_chrom)  #not necessary
   gtf <- data.frame(
     seqname=Tab$tx_chrom, source= c(rep("TxDb")),feature= c(rep("exon")),
-    start=Tab$start,end=Tab$tx_end,score=c(rep(".")),strand=Tab$strand,
-    frame=c(rep(".")), attributes= paste(Tab$gene_id,Tab$tx_name,Tab$exon_rank,sep="; ")
+    start=Tab$start,end=Tab$end,score=c(rep(".")),strand=Tab$strand,
+    frame=c(rep(".")), attributes= paste(Tab$gene_id,Tab$tx_name,Tab$exon_rank,Tab$exon_id)
   )
   return(gtf)
 }
